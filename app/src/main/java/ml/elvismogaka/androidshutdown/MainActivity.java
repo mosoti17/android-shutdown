1ce879e47dd6a54337760aa7fcd11da260a63743
mosoti17 <mosoti17@gmail.com>
2018-02-01 13:49:19 +0300

    added dialog for users who are not rooted

--- app/src/main/java/ml/elvismogaka/androidshutdown/MainActivity.java
+++ app/src/main/java/ml/elvismogaka/androidshutdown/MainActivity.java
@@ -17,37 +17,34 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView shutdown;
-    ProgressBar progressBar;
-    TextView shutdowntext;
+    private ImageView shutdown;
+    private TextView shutdowntext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         shutdown= findViewById(R.id.shutdown);
-        progressBar = findViewById(R.id.progressbar);
         shutdowntext= findViewById(R.id.shutdowntext);
 
-        progressBar.setVisibility(View.GONE);
 
 
 
         shutdown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 try {
+                    shutdown.setColorFilter(getResources().getColor(R.color.colorPrimary));
+                    Toast.makeText(getApplicationContext(),"Shutting down ...",Toast.LENGTH_LONG).show();
                     Process proc = Runtime.getRuntime()
                             .exec(new String[]{ "su", "-c", "reboot -p" });
 
 
                     proc.waitFor();
-                    shutdown.setColorFilter(getResources().getColor(R.color.colorPrimary));
-                    progressBar.setVisibility(View.VISIBLE);
-
-
 
                 } catch (Exception ex) {
+
                     Toast.makeText(getApplicationContext(),"This application only works with rooted devices. Root your device to be able to use this application",Toast.LENGTH_LONG).show();
 
                     ex.printStackTrace();
