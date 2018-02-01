09132dc30f6533690605c214a19094ddf961b18b
mosoti17 <mosoti17@gmail.com>
2018-02-01 10:50:05 +0300

    added toast for users who dont have rooted devices

--- app/src/main/java/ml/elvismogaka/androidshutdown/MainActivity.java
+++ app/src/main/java/ml/elvismogaka/androidshutdown/MainActivity.java
@@ -1,5 +1,6 @@
 package ml.elvismogaka.androidshutdown;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
@@ -8,18 +9,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     ImageView shutdown;
+    ProgressBar progressBar;
+    TextView shutdowntext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         shutdown= findViewById(R.id.shutdown);
+        progressBar = findViewById(R.id.progressbar);
+        shutdowntext= findViewById(R.id.shutdowntext);
+
+        progressBar.setVisibility(View.GONE);
+
+
 
         shutdown.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,9 +39,17 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     Process proc = Runtime.getRuntime()
                             .exec(new String[]{ "su", "-c", "reboot -p" });
+
+
                     proc.waitFor();
+                    shutdown.setColorFilter(getResources().getColor(R.color.colorPrimary));
+                    progressBar.setVisibility(View.VISIBLE);
+
+
 
                 } catch (Exception ex) {
+                    Toast.makeText(getApplicationContext(),"This application only works with rooted devices. Root your device to be able to use this application",Toast.LENGTH_LONG).show();
+
                     ex.printStackTrace();
                 }
 
